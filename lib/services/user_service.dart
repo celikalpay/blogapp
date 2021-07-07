6ddda7a46f8389dcd2953d1956b8f2100d73addb
alpaycelik <alpay.celik@bil.omu.edu.tr>
2021-07-08 00:09:09 +0300

    user profile edit

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -72,6 +72,45 @@ Future<ApiResponse> register(String name, String email, String password) async {
   return apiResponse;
 }
 
+Future<ApiResponse> updateUser(String name, String? image) async {
+  ApiResponse apiResponse = ApiResponse();
+  try {
+    String token = await getToken();
+    final response = await http.put(
+      Uri.parse(userURL),
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+      body: image == null
+          ? {
+              'name': name,
+            }
+          : {
+              'name': name,
+              'image': image,
+            },
+    );
+    // user can update his/her name or name and image
+
+    switch (response.statusCode) {
+      case 200:
+        apiResponse.data = jsonDecode(response.body)['message'];
+        break;
+      case 401:
+        apiResponse.error = unauthorized;
+        break;
+      default:
+        apiResponse.error = somethingWhenWrong;
+        break;
+    }
+  } catch (e) {
+    apiResponse.error = serverError;
+  }
+
+  return apiResponse;
+}
+
 // User
 Future<ApiResponse> getUserDetail() async {
   ApiResponse apiResponse = ApiResponse();
